6bf30afe7f3c84c50d9da3c4c85b2dee5f879879
Yong Kit <youremail@example.com>
2026-03-07 21:26:33 +0800

    update

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -140,5 +140,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'frontend/static')
 ]
 
-ALLOWED_HOSTS=['localhost','https://yk942003-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://yk942003-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=['localhost','https://yk942003-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://yk942003-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
