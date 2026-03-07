d79bdb3c9b0f0cc0158e7c016c53161c22ec55c3
Yong Kit <youremail@example.com>
2026-03-07 22:15:43 +0800

    Authentication

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,7 +62,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -137,7 +139,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+	os.path.join(BASE_DIR, 'frontend/build'),
+	os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 ALLOWED_HOSTS=['localhost','https://yk942003-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('logout/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('djangoapp/', include('djangoapp.urls')),
